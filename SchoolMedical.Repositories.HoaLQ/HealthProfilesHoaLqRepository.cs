9721d86cb7a0a5c572f99b6a31d1597ca38198b9
quanghoaluu1 <hoalqse183138@fpt.edu.vn>
2025-06-16 16:57:43 +0700

    add

---
+++ SchoolMedical.Repositories.HoaLQ/HealthProfilesHoaLqRepository.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchoolMedical.Repositories.HoaLQ.Basic;
+using SchoolMedical.Repositories.HoaLQ.Models;
+
+namespace SchoolMedical.Repositories.HoaLQ
+{
+    public class HealthProfilesHoaLqRepository: GenericRepository<HealthProfilesHoaLq>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public HealthProfilesHoaLqRepository()
+        {
+            _context ??= new SU25_PRN222_SE1709_G1_SchoolMedicalContext();
+        }
+
+        public HealthProfilesHoaLqRepository(SU25_PRN222_SE1709_G1_SchoolMedicalContext context)
+        {
+            _context = context;
+        }
+        
+        public new async Task< List<HealthProfilesHoaLq>> GetAllAsync()
+        {
+            return await _context.HealthProfilesHoaLqs
+                .Include(h => h.Student)
+                .ToListAsync() ?? new List<HealthProfilesHoaLq>();
+        }
+
+        public async Task<HealthProfilesHoaLq> GetByIdAsync(int id)
+        {
+            var healthProfile = await _context.HealthProfilesHoaLqs
+                .Include(h => h.Student)
+                .FirstOrDefaultAsync(h => h.HealthProfileHoaLqid == id);
+            return healthProfile ?? new HealthProfilesHoaLq();
+        }
+
+        public async Task<List<HealthProfilesHoaLq>> SearchAsync(string bloodType, string searchCode, int sight)
+        {
+            var healthProfiles = await _context.HealthProfilesHoaLqs.Include(h => h.Student).Where(h => (h.BloodType.Contains(bloodType) || string.IsNullOrEmpty(bloodType))
+            && (h.Student.StudentCode.Contains(searchCode) || string.IsNullOrEmpty(searchCode))
+            && (h.Sight == sight || sight == 0))
+                .ToListAsync();
+
+            return healthProfiles ?? new List<HealthProfilesHoaLq>();
+        }
+        
+        public new async Task<HealthProfilesHoaLq> CreateAsync(HealthProfilesHoaLq healthProfilesHoaLq)
+        {
+            var entity = _context.HealthProfilesHoaLqs.Add(healthProfilesHoaLq);
+            await _context.SaveChangesAsync();
+
+            var result = await _context.HealthProfilesHoaLqs
+                .Where(h => h.HealthProfileHoaLqid == entity.Entity.HealthProfileHoaLqid)
+                .Select(h => new HealthProfilesHoaLq
+                {
+                    HealthProfileHoaLqid = h.HealthProfileHoaLqid,
+                    Weight = h.Weight,
+                    Height = h.Height,
+                    BloodPressure = h.BloodPressure,
+                    Allergy = h.Allergy,
+                    ChronicDisease = h.ChronicDisease,
+                    MedicalHistory = h.MedicalHistory,
+                    CurrentMedical = h.CurrentMedical,
+                    BloodType = h.BloodType,
+                    Sight = h.Sight,
+                    Hearing = h.Hearing,
+                    Sex = h.Sex,
+                    DateOfBirth = h.DateOfBirth,
+                    StudentId = h.StudentId,
+            
+                    Student = new StudentsHoaLq()
+                    {
+                        StudentsHoaLqid = h.Student.StudentsHoaLqid,
+                        StudentFullName = h.Student.StudentFullName
+                    }
+                })
+                .FirstOrDefaultAsync();
+
+            return result;
+        }
+
+        public new async Task<HealthProfilesHoaLq> UpdateAsync(HealthProfilesHoaLq healthProfilesHoaLq)
+        {
+            var entity = _context.HealthProfilesHoaLqs.Update(healthProfilesHoaLq);
+            await _context.SaveChangesAsync();
+
+            var result = await _context.HealthProfilesHoaLqs
+                .Where(h => h.HealthProfileHoaLqid == entity.Entity.HealthProfileHoaLqid)
+                .Select(h => new HealthProfilesHoaLq
+                {
+                    HealthProfileHoaLqid = h.HealthProfileHoaLqid,
+                    Weight = h.Weight,
+                    Height = h.Height,
+                    BloodPressure = h.BloodPressure,
+                    Allergy = h.Allergy,
+                    ChronicDisease = h.ChronicDisease,
+                    MedicalHistory = h.MedicalHistory,
+                    CurrentMedical = h.CurrentMedical,
+                    BloodType = h.BloodType,
+                    Sight = h.Sight,
+                    Hearing = h.Hearing,
+                    Sex = h.Sex,
+                    DateOfBirth = h.DateOfBirth,
+                    StudentId = h.StudentId,
+
+                    Student = new StudentsHoaLq()
+                    {
+                        StudentsHoaLqid = h.Student.StudentsHoaLqid,
+                        StudentFullName = h.Student.StudentFullName
+                    }
+                })
+                .FirstOrDefaultAsync();
+
+            return result;
+        }
+    }
+}
---
+++ SchoolMedical.Services.HoaLQ/HealthProfilesHoaLqService.cs
@@ -0,0 +1,56 @@
+﻿using SchoolMedical.Repositories.HoaLQ;
+using SchoolMedical.Repositories.HoaLQ.Models;
+
+namespace SchoolMedical.Services.HoaLQ;
+
+public interface IHealthProfilesHoaLqService
+{
+    Task<List<HealthProfilesHoaLq>> GetAllAsync();
+    Task<HealthProfilesHoaLq> GetByIdAsync(int id);
+    Task<List<HealthProfilesHoaLq>> SearchAsync(string bloodType, string searchCode, int sight);
+    Task<HealthProfilesHoaLq> CreateAsync(HealthProfilesHoaLq healthProfilesHoaLq);
+    Task<HealthProfilesHoaLq> UpdateAsync(HealthProfilesHoaLq healthProfilesHoaLq);
+    Task<bool> RemoveAsync(int id);
+}
+public class HealthProfilesHoaLqService: IHealthProfilesHoaLqService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public HealthProfilesHoaLqService()
+    {
+        _unitOfWork  ??= new UnitOfWork();
+    }
+
+    public async Task<List<HealthProfilesHoaLq>> GetAllAsync()
+    {
+        return await _unitOfWork.HealthProfilesHoaLqRepository.GetAllAsync();
+    }
+    
+    public async Task<HealthProfilesHoaLq> GetByIdAsync(int id)
+    {
+        return await _unitOfWork.HealthProfilesHoaLqRepository.GetByIdAsync(id);
+    }
+    
+    public async Task<List<HealthProfilesHoaLq>> SearchAsync(string bloodType, string searchCode, int sight)
+    {
+        return await _unitOfWork.HealthProfilesHoaLqRepository.SearchAsync(bloodType, searchCode, sight);
+    }
+    
+    public async Task<HealthProfilesHoaLq> CreateAsync(HealthProfilesHoaLq healthProfilesHoaLq)
+    {
+        var createdProfile = await _unitOfWork.HealthProfilesHoaLqRepository.CreateAsync(healthProfilesHoaLq);
+        Console.WriteLine(createdProfile);
+        return createdProfile;
+    }
+    
+    public async Task<HealthProfilesHoaLq> UpdateAsync(HealthProfilesHoaLq healthProfilesHoaLq)
+    {
+        var updatedProfile = await _unitOfWork.HealthProfilesHoaLqRepository.UpdateAsync(healthProfilesHoaLq);
+        return updatedProfile;
+    }
+    
+    public async Task<bool> RemoveAsync(int id)
+    {
+        var removedProfile = await _unitOfWork.HealthProfilesHoaLqRepository.GetByIdAsync(id);
+        return await _unitOfWork.HealthProfilesHoaLqRepository.RemoveAsync(removedProfile);
+    }
+}
